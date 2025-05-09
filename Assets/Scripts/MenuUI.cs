9efff2da4e0a3ef4f9405f0f51884970bbd8849a
MattD04 <118762220+MattD04@users.noreply.github.com>
2025-05-09 19:25:42 -0400

    Exit Button Updated
    Updated so that the Exit Buttons Exits game and Editor Playmode

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -13,4 +14,15 @@ public class MenuUI : MonoBehaviour
         DataManager.Instance.playerName = nameinputField.text;
         SceneManager.LoadScene(1);
     }
+
+    public void Exit()
+    {
+
+#if UNITY_EDITOR //# isnt' "code" persay, but rather telling the editor what to do, so our exit button will exit the play mode
+        EditorApplication.ExitPlaymode();
+#else
+               Application.Quit(); // original code to quit Unity when the player is playing the game
+#endif
+    }
+
 }
